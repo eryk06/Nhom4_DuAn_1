b75b15a72289d9aa52f6a5d193df32e51e930da2
PLong2003 <99625263+PLong2003@users.noreply.github.com>
2022-11-10 08:42:51 +0700

    halo

--- app/src/main/java/com/example/nhom4_duan_1/MainActivity.java
+++ app/src/main/java/com/example/nhom4_duan_1/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("halo");
+        System.out.println("halo");
     }
 }
