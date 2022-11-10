ace7fc57cf12c6301f8dc9e63616c8780b442f44
Vanilla23 <lamvip06192003@gmail.com>
2022-11-10 07:54:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/nhom4_duan_1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.nhom4_duan_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/nhom4_duan_1/views/AccountActivity.java
@@ -0,0 +1,16 @@
+package com.example.nhom4_duan_1.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nhom4_duan_1.R;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
