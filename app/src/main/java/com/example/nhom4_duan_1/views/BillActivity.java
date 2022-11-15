8ce08a01585f187f108e5945774dcc2808cd1ec4
PLong2003 <99625263+PLong2003@users.noreply.github.com>
2022-11-15 08:45:51 +0700

    views

---
+++ app/src/main/java/com/example/nhom4_duan_1/views/BillActivity.java
@@ -0,0 +1,16 @@
+package com.example.nhom4_duan_1.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.nhom4_duan_1.R;
+
+public class BillActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill);
+
+    }
+}
