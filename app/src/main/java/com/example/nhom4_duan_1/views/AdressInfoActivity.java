a050a5f69ffe09a7d79849406816430803b35ba1
Vanilla23 <lamvip06192003@gmail.com>
2022-11-17 07:45:08 +0700

    add layout 3

--- app/src/main/java/com/example/nhom4_duan_1/views/AdressInfoActivity.java
+++ app/src/main/java/com/example/nhom4_duan_1/views/AdressInfoActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.nhom4_duan_1.R;
 
-public class BillActivity extends AppCompatActivity {
+public class AdressInfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bill);
+        setContentView(R.layout.activity_adress_info);
     }
 }
