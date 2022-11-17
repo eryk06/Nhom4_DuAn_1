7fd77478eb462e219ad55019678e4c178ab809ae
hq-dat <dathqps22895@fpt.edu.vn>
2022-11-17 09:21:47 +0700

    properti

--- app/src/main/java/com/example/nhom4_duan_1/views/HiActivity.java
+++ app/src/main/java/com/example/nhom4_duan_1/views/HiActivity.java
@@ -2,8 +2,11 @@ package com.example.nhom4_duan_1.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.nhom4_duan_1.MainActivity;
 import com.example.nhom4_duan_1.R;
 
 public class HiActivity extends AppCompatActivity {
@@ -12,5 +15,11 @@ public class HiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hi);
+
+    }
+
+    public void getContinue(View view){
+        Intent intent = new Intent(HiActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/nhom4_duan_1/views/WelcomeActivity.java
+++ app/src/main/java/com/example/nhom4_duan_1/views/WelcomeActivity.java
@@ -2,7 +2,9 @@ package com.example.nhom4_duan_1.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 import com.example.nhom4_duan_1.R;
 
@@ -12,5 +14,18 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        CountDownTimer timer = new CountDownTimer(3000,3000) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(WelcomeActivity.this, HiActivity.class);
+                startActivity(intent);
+            }
+        }.start();
     }
 }
