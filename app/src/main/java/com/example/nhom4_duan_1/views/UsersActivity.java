6bcf3ee9aeeaa9d813966334fe92f1fa48833cf9
Vanilla23 <lamvip06192003@gmail.com>
2022-11-20 22:05:41 +0700

    add layout 4

---
+++ app/src/main/java/com/example/nhom4_duan_1/views/UsersActivity.java
@@ -0,0 +1,16 @@
+package com.example.nhom4_duan_1.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nhom4_duan_1.R;
+
+public class UsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users);
+    }
+}
