039070b13f4589d9ccb493d88e6fbd2aac897abc
HuyMatec <huythps22731@fpt.edu.vn>
2022-11-22 02:28:36 +0700

    adapter and activity

---
+++ app/src/main/java/com/example/nhom4_duan_1/adapters/CartAdapter.java
@@ -0,0 +1,55 @@
+package com.example.nhom4_duan_1.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.nhom4_duan_1.R;
+import com.example.nhom4_duan_1.models.Bills;
+
+import java.util.ArrayList;
+
+public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder>{
+    private Context context;
+    private ArrayList<Bills> list;
+
+    public CartAdapter(Context context, ArrayList<Bills> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+        View view = inflater.inflate(R.layout.item_product_cart, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+
+
+        }
+    }
+}
--- app/src/main/java/com/example/nhom4_duan_1/views/CartActivity.java
+++ app/src/main/java/com/example/nhom4_duan_1/views/CartActivity.java
@@ -1,16 +1,21 @@
 package com.example.nhom4_duan_1.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.nhom4_duan_1.R;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class CartActivity extends AppCompatActivity {
-
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    RecyclerView recyclerCart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+
     }
 }
