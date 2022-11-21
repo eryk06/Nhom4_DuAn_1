d0296a7eff2b0f2532d054d981d62be2ed7d7d41
HuyMatec <huythps22731@fpt.edu.vn>
2022-11-22 01:46:09 +0700

    adapter and activity

--- app/src/main/java/com/example/nhom4_duan_1/views/UsersActivity.java
+++ app/src/main/java/com/example/nhom4_duan_1/views/UsersActivity.java
@@ -1,10 +1,13 @@
 package com.example.nhom4_duan_1.views;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.example.nhom4_duan_1.MainActivity;
@@ -13,6 +16,8 @@ import com.example.nhom4_duan_1.models.Vouchers;
 
 public class UsersActivity extends AppCompatActivity {
     LinearLayout llMO, llVouchers, llPM, llTP;
+    ImageView ivUser;
+    private final int GALLERY_REQ_CODE = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +27,16 @@ public class UsersActivity extends AppCompatActivity {
         llVouchers = (LinearLayout) findViewById(R.id.llVouchers);
         llPM = (LinearLayout) findViewById(R.id.llPM);
         llTP = (LinearLayout) findViewById(R.id.llTP);
+        ivUser = (ImageView) findViewById(R.id.ivUser);
+
+        ivUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, GALLERY_REQ_CODE);
+            }
+        });
 
         llMO.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,5 +69,18 @@ public class UsersActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode==RESULT_OK){
+
+            if (requestCode==GALLERY_REQ_CODE){
+                ivUser.setImageURI(data.getData());
+            }
+
+        }
     }
 }
