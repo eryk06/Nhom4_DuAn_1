b18af75ea1a42e6e7a56541b966f17cc88a2b111
PLong2003 <99625263+PLong2003@users.noreply.github.com>
2022-11-22 04:00:34 +0700

    DetailProduct

--- app/src/main/java/com/example/nhom4_duan_1/views/CartActivity.java
+++ app/src/main/java/com/example/nhom4_duan_1/views/CartActivity.java
@@ -15,7 +15,5 @@ public class CartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
-
-
     }
 }
