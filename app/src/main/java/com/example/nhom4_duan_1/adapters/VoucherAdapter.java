47b6dd54450c9aa34057d6ffc7d4052e22937a1c
HuyMatec <huythps22731@fpt.edu.vn>
2022-11-21 23:17:03 +0700

    adapter and activity

--- app/src/main/java/com/example/nhom4_duan_1/adapters/VoucherAdapter.java
+++ app/src/main/java/com/example/nhom4_duan_1/adapters/VoucherAdapter.java
@@ -12,14 +12,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.nhom4_duan_1.R;
 import com.example.nhom4_duan_1.models.Bills;
+import com.example.nhom4_duan_1.models.Vouchers;
 
 import java.util.ArrayList;
 
 public class VoucherAdapter extends RecyclerView.Adapter<VoucherAdapter.ViewHolder>{
     private Context context;
-    private ArrayList<Bills> list;
+    private ArrayList<Vouchers> list;
 
-    public VoucherAdapter(Context context, ArrayList<Bills> list) {
+    public VoucherAdapter(Context context, ArrayList<Vouchers> list) {
         this.context = context;
         this.list = list;
     }
@@ -28,18 +29,15 @@ public class VoucherAdapter extends RecyclerView.Adapter<VoucherAdapter.ViewHold
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = ((Activity)context).getLayoutInflater();
-        View view = inflater.inflate(R.layout.item_oder, parent, false);
+        View view = inflater.inflate(R.layout.item_voucher, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.tvTime.setText(list.get(position).getTime());
-        holder.tvPrice.setText(list.get(position).getPrice());
-        holder.tvAmount.setText(list.get(position).getAmount());
+        holder.tvName.setText(list.get(position).getName());
+
     }
-
-
 
     @Override
     public int getItemCount() {
@@ -47,11 +45,11 @@ public class VoucherAdapter extends RecyclerView.Adapter<VoucherAdapter.ViewHold
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-        TextView tvTime, tvPrice, tvAmount, tvRepurchase;
+        TextView tvName, tvChoose;
         public ViewHolder(@NonNull View itemView){
             super(itemView);
-
-            //jihuihuihuuihui
+            tvName = itemView.findViewById(R.id.tvName);
+            tvChoose = itemView.findViewById(R.id.tvChoose);
         }
     }
 }
--- app/src/main/java/com/example/nhom4_duan_1/views/VoucherActivity.java
+++ app/src/main/java/com/example/nhom4_duan_1/views/VoucherActivity.java
@@ -1,18 +1,70 @@
 package com.example.nhom4_duan_1.views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.nhom4_duan_1.R;
+import com.example.nhom4_duan_1.adapters.OderAdapter;
+import com.example.nhom4_duan_1.adapters.VoucherAdapter;
+import com.example.nhom4_duan_1.models.Bills;
+import com.example.nhom4_duan_1.models.Vouchers;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class VoucherActivity extends AppCompatActivity {
-
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    RecyclerView recyclerVoucher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voucher);
 
+        recyclerVoucher = (RecyclerView) findViewById(R.id.recyclerVoucher);
+        getData();
+    }
+    public void getData(){
+        ArrayList<Vouchers> list = new ArrayList<>();
+        db.collection("Vouchers")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            int i = 0;
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                i+=1;
+                                Map<String, Object> item = document.getData();
+                                Vouchers vouchers = new Vouchers();
+                                vouchers.setId(document.getId());
+                                vouchers.setName(item.get("Name").toString());
+                                vouchers.setPrice(Double.parseDouble(item.get("Price").toString()));
+                                list.add(vouchers);
+                                System.out.println(i + " ---" + list.get(list.size()-1));
+                            }
+                            FillData(list);
+                        } else {
+                            Log.w(">>>TAG", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+    }
 
+    public void FillData(ArrayList<Vouchers> list){
+        recyclerVoucher = (RecyclerView) findViewById(R.id.recyclerVoucher);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(VoucherActivity.this);
+        recyclerVoucher.setLayoutManager(linearLayoutManager);
+        VoucherAdapter adapter = new VoucherAdapter(VoucherActivity.this, list);
+        recyclerVoucher.setAdapter(adapter);
     }
 }
