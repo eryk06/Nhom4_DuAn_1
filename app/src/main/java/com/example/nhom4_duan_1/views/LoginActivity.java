5f2ea1682ee652a6957793d91fa7def64fc09369
PLong2003 <99625263+PLong2003@users.noreply.github.com>
2022-11-24 01:12:05 +0700

    Login

--- app/src/main/java/com/example/nhom4_duan_1/views/LoginActivity.java
+++ app/src/main/java/com/example/nhom4_duan_1/views/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -13,18 +14,30 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.nhom4_duan_1.MainActivity;
 import com.example.nhom4_duan_1.R;
+import com.example.nhom4_duan_1.models.Users;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.auth.User;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     private GoogleSignInClient googleSignInClient;
     private SignInButton signInButton;
+    ArrayList<Users> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +46,8 @@ public class LoginActivity extends AppCompatActivity {
         EditText edtSDT = (EditText) findViewById(R.id.edtSDT);
         Button btnContinue = (Button) findViewById(R.id.btnContinue);
 
+        list = new ArrayList<>();
+        getUser();
         signInButton = findViewById(R.id.btnGoogle);
 
         // google
@@ -66,8 +81,28 @@ public class LoginActivity extends AppCompatActivity {
                     Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
                     try {
                         GoogleSignInAccount account = task.getResult(ApiException.class);
-                        String email = account.getEmail();
-                        Log.d(">>>>>>TAG", "onActivityResult Email: "  + email);
+                        String pass = account.getId();
+                        int check = 0;
+                        Users userCheck = new Users();
+                        for (Users user1: list) {
+                            if (user1.getPass().equals(pass)){
+                                check = 1;
+                                userCheck = user1;
+                                break;
+                            }
+                        }
+                        if (check == 1){
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            intent.putExtra("Id",userCheck.getId());
+                            intent.putExtra("Login","email");
+                            startActivity(intent);
+                        }
+                        else {
+                            Intent intent = new Intent(LoginActivity.this, LoginEmailActivity.class);
+                            intent.putExtra("Pass",pass);
+                            intent.putExtra("Login","email");
+                            startActivity(intent);
+                        }
                     } catch (Exception e) {
                         Log.d(">>>>>>TAG", "onActivityResult Error: " + e.getMessage());
                     }
@@ -75,4 +110,31 @@ public class LoginActivity extends AppCompatActivity {
             }
     );
 
+    public void getUser(){
+        db.collection("Users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            int i = 0;
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                i+=1;
+                                Map<String, Object> item = document.getData();
+                                Users user = new Users();
+                                user.setId(document.getId());
+                                user.setName(item.get("Name").toString());
+                                user.setImage(item.get("Image").toString());
+                                user.setPass(item.get("Pass").toString());
+                                user.setPhone(item.get("Phone").toString());
+                                user.setAddress(item.get("Address").toString());
+                                list.add(user);
+                                System.out.println(i + " ---" + list.get(list.size()-1));
+                            }
+                        } else {
+                            Log.w(">>>TAG", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+    }
 }
